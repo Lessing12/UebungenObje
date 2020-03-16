0b914955726735cfca78cfe16aed5de9f3b5ec04
Marcin <mgabrys422@gmail.com>
2020-03-16 14:21:53 +0100

    Objekte

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+let europaeischeUnion = {
+
+    Belgien: "BE",
+    Griechenland: "EL",
+    Litauen: "LT",
+    Portugel: "PT",
+    Bulgarien: "BG",
+    Spanien: "ES",
+    Luxemburg: "LU",
+    Rumaenien: "RO",
+    Tschechien: "CZ",
+    Frankreich: "FR",
+    Ungarn: "HU",
+    Slowenien: "SI",
+    Dänemark: "DK",
+    Kroatien: "HR",
+    Malta: "MT",
+    Slowenien: "SK",
+    Deutschland: "DE",
+    Italien: "IT",
+    Niederlande: "NL",
+    Finnland: "FI",
+    Estland: "EE",
+    Zypern: "CY",
+    Österreich: "AT",
+    Schweden: "SE",
+    Irland: "IE",
+    Lettland: "LV",
+    Polen: "PL",
+    VereinigtesKoenigreich: "UK"
+
+}
+
+console.log(europaeischeUnion);
+
+let BeNeLuxUnion = {
+    Belgien: "BE",
+    Niederlande: "NE",
+    Luxemburg: "LU",
+}
+console.log(BeNeLuxUnion);
